9534f87cf70cec04ab705f3dc8db2ef6bee3b572
Tan Nguyen <tan@fitwp.com>
2015-12-12 14:14:50 +0700

    Add login feature

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -22,13 +22,13 @@ Route::get('profile', function () {
     return view('users/profile');
 });
 
-Route::get('auth/login', 'Auth\AuthController@getLogin');
-Route::post('auth/login', 'Auth\AuthController@postLogin');
-Route::get('auth/logout', 'Auth\AuthController@getLogout');
+Route::get('login', 'Auth\AuthController@getLogin');
+Route::post('login', 'Auth\AuthController@postLogin');
+Route::get('logout', 'Auth\AuthController@getLogout');
 
 // Registration Routes...
-Route::get('auth/register', 'Auth\AuthController@getRegister');
-Route::post('auth/register', 'Auth\AuthController@postRegister');
+Route::get('register', 'Auth\AuthController@getRegister');
+Route::post('register', 'Auth\AuthController@postRegister');
 
 
 Route::resource('branches', 'BranchController');
