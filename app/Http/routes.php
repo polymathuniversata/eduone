b58082279c56c231f9174c357de946a6444b80e0
Tan Nguyen <tan@binaty.org>
2016-04-12 23:09:51 +0700

    Requires login to view dashboard

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,50 +11,48 @@
 |
 */
 
-Route::get('/', function () {
-	// if ( ! Auth::user())
-	// 	Auth::loginUsingId(1);
+Route::group(['middleware' => 'auth'], function () {
+	Route::get('/', function () {
+	    return view('dashboard');
+	});
 
-    return view('dashboard');
+	Route::get('profile', function () {
+	    return view('users/profile');
+	});
+
+	Route::get('login', 'Auth\AuthController@getLogin');
+	Route::post('login', 'Auth\AuthController@postLogin');
+	Route::get('logout', 'Auth\AuthController@getLogout');
+
+	Route::get('profile', 'UserController@profile');
+
+	// Registration Routes...
+	Route::get('register', 'Auth\AuthController@getRegister');
+	Route::post('register', 'Auth\AuthController@postRegister');
+
+	Route::resource('branches', 'BranchController');
+
+	Route::get('settings/grades', 'SettingController@grades');
+	Route::resource('settings', 'SettingController');
+
+	Route::resource('rooms', 'RoomController');
+	Route::resource('roles', 'RoleController');
+	Route::get('classes/{id}/subjects', 'ClassController@subjects');
+	Route::get('classes/{id}/subjects/{subject_id}', 'ClassController@teacher');
+	Route::resource('classes', 'ClassController');
+	Route::resource('subjects', 'SubjectController');
+	Route::resource('programs', 'ProgramController');
+	Route::get('users/search', 'UserController@search');
+
+	Route::resource('users', 'UserController');
+	Route::resource('media', 'MediaController');
+	Route::resource('schedules', 'ScheduleController');
+	Route::resource('attendances', 'AttendanceController');
+	Route::resource('grades', 'GradeController');
+	Route::resource('transports', 'TransportController');
 });
 
-Route::get('profile', function () {
-    return view('users/profile');
-});
 
-Route::get('login', 'Auth\AuthController@getLogin');
-Route::post('login', 'Auth\AuthController@postLogin');
-Route::get('logout', 'Auth\AuthController@getLogout');
 
-Route::get('profile', 'UserController@profile');
-
-// Registration Routes...
-Route::get('register', 'Auth\AuthController@getRegister');
-Route::post('register', 'Auth\AuthController@postRegister');
-
-Route::resource('branches', 'BranchController');
-
-Route::get('settings/grades', 'SettingController@grades');
-Route::resource('settings', 'SettingController');
-
-Route::resource('rooms', 'RoomController');
-Route::resource('roles', 'RoleController');
-Route::get('classes/{id}/subjects', 'ClassController@subjects');
-Route::get('classes/{id}/subjects/{subject_id}', 'ClassController@teacher');
-Route::resource('classes', 'ClassController');
-Route::resource('subjects', 'SubjectController');
-Route::resource('programs', 'ProgramController');
-Route::get('users/search', 'UserController@search');
-
-Route::resource('users', 'UserController');
-Route::resource('media', 'MediaController');
-Route::resource('schedules', 'ScheduleController');
-Route::resource('attendances', 'AttendanceController');
-Route::resource('grades', 'GradeController');
-Route::resource('transports', 'TransportController');
 
 Route::auth();
-
-Route::get('/home', 'HomeController@index');
-
-Route::get('fack', 'UserController@profile');
