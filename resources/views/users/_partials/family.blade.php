b1f498a093625630efb46c48ba18303a7a7396ae
Tan Nguyen <tan@binaty.org>
2016-04-13 22:51:43 +0700

    Only shows not found template when user typing

--- resources/views/users/_partials/family.blade.php
+++ resources/views/users/_partials/family.blade.php
@@ -62,7 +62,7 @@ var searchParams = {};
 			<li ng-repeat="user in users track by $index" ng-click="addUser($index)">
 				<img ng-src="@{{user.photo}}"> @{{user.display_name}}
 			</li>
-			<li class="not-found" ng-show="users.length == 0">Sorry we cannot found any user.</li>
+			<li class="not-found" ng-show="users.length == 0 && search.length > 0">Sorry we cannot found any user.</li>
 		</ul>
 
 		<input type="hidden" name="family_members" value="@{{queue}}">
